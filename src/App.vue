06eeece22a1c509e1b873d3dfa8d5724226c36c6
Chuloo <williamimoh@yahoo.com>
2017-10-26 15:26:00 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header name="The Vue App"></Header>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components:{Header},
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template functional>
+  <div>
+      <h1>{{props.name}}</h1>
+      <p>Hello and welcome to your sample Vue App, feel free to edit it to whichever way you choose and have fun!</p>
+  </div>
+</template>
+
+<style scoped>
+    div>h1{
+        color:#333;
+        text-align:center;
+    }
+</style>
